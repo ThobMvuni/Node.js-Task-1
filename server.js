e7589fd563e383d2febb24d32f3f7111bdf05f90
Thobile Mvuni <thoyomvuni@gmail.com>
2025-04-24 00:17:05 +0200

    Added files

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 const http = require('node:http')
 
 const hostname = '127.0.0.1'
-const port = 3001
+const port = 3000
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { ensureDataFile, readData, writeData } = require('./data_handler');
+
+const app = express();
+
+ensureDataFile();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/data', (req, res) => {
+  const existingData = readData();
+  existingData.push(req.body);
+  writeData(existingData);
+  res.status(201).json({ message: "Data saved successfully!" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running `);
+});
+
+
+
 
 let movies = ['Hangover', 'Harry Porter','Avengers', 'Wonder woman']
 let series = ['Game of Thrones', 'The middle', 'Suits', 'Veep']
